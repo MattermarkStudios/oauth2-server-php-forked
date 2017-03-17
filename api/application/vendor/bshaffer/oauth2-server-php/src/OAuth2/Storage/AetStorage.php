09ea5a235cef11ec04f3c93247f1bc87da0eeffd
Teodor Talov <teodor@mattermark.us>
2017-03-17 04:03:10 -0400

    Create AetStorage.php

---
+++ api/application/vendor/bshaffer/oauth2-server-php/src/OAuth2/Storage/AetStorage.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace OAuth2\Storage;
+
+class AetStorage implements
+    AccessTokenInterface,
+    ClientCredentialsInterface, 
+    UserCredentialsInterface
+{
+    
+
+    public function __construct($config = array())
+    {
+  
+    }
+
+    }
+}
